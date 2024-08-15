522df579418c5875174d2868f0c52afd16541a80
Ashraf Hashimy <sahashimy1@gmail.com>
2024-08-15 07:15:14 +0100

    first-commit

--- main.tf
+++ main.tf
@@ -1,20 +1,10 @@
 provider "aws" {
   region = "eu-west-1"
 }
-
-# resource "aws_instance" "example" {
-#   ami           = "ami-0e91d7b0085dbf012"
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = "ExampleInstance"
-#   }
-# }
-
 terraform {
   backend "s3" {
     bucket = "ash-ter-cert-bucket-2024"
-    key    = "TFStatefile/terraform.tfstate"
+    key    = "Platform/dev/infrastructure/terraform.tfstate"
     region = "eu-west-1"
   }
 }
